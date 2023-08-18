1aefd21ab4b4b2331698bea60fbe17d6e4a1867a
Md.Solaiman <md.solaiman366882@gmail.com>
2023-08-19 02:45:54 +0600

    All problem Solved like counting problem and new style added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 document.getElementById('add-btn').addEventListener('click',function(event){
     const inputValue = document.getElementById('task-input').value;
-    const elementCount = document.getElementById('task-list').childElementCount;
+    // const elementCount = document.getElementById('task-list').childElementCount;
     const li = document.createElement('li');
     li.setAttribute('class','single-task');
     // ${elementCount}
     li.innerHTML = `
-        <h2 class="task-number">${elementCount}</h2>
+        <h2 class="task-number"></h2>
         <p class="task-description"></p>
         <div class="action-btn-area">
             <button class="btn" onclick="doneCurrentTask()">done</button>
@@ -21,21 +21,31 @@ document.getElementById('add-btn').addEventListener('click',function(event){
 
 
 
-    // const lists = document.querySelectorAll('#task-list li');
-    // let count = 1;
-    //  for(list of lists) {
-    //     const h2 = document.querySelector('.single-task > h2');
-    //     h2.innerText = count;
-    //     count++;
-    //     console.log(h2);
+    const lists = document.querySelectorAll('#task-list li.single-task h2');
+    let count = 1;
+     for( list of lists) {
+        list.innerText = count;
+        count++
 
-    //  }
+     }
 });
 function doneCurrentTask(){
     event.target.parentNode.previousElementSibling.style.textDecoration='line-through';
+    event.target.parentNode.previousElementSibling.style.textDecorationColor='lime';
+    event.target.parentNode.previousElementSibling.style.textDecorationThickness='25%';
+    event.target.parentNode.previousElementSibling.style.textDecorationStyle='dashed';
+    event.target.parentNode.previousElementSibling.setAttribute('title','completed');
 }
 function deleteCurrentTask(){
     event.target.parentNode.parentNode.remove();
+
+    const lists = document.querySelectorAll('#task-list li.single-task h2');
+    let count = 1;
+     for( list of lists) {
+        list.innerText = count;
+        count++
+
+     }
 };
 // document.getElementById('add-btn').addEventListener('click',function(){
 
